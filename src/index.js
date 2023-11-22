4781e5af04b1d23fe54cadaa3c81c3a48ba55b5c
d-ecember <119058761+d-ecember@users.noreply.github.com>
2023-11-22 21:14:58 +0100

    Added the Forecast

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function updateWeatherData(response) {
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeELement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForcast(response.data.city);
 }
 
 function searchCity(city) {
@@ -48,6 +50,45 @@ function formatDate(date) {
   }
 
   return `${day} ${hours}:${minute}`;
+}
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}º</div>
+          </div>
+        </div>
+      `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+function getForcast(city) {
+  let apiKey = "c346o99849899025a5ea673a4f03tb46";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
 }
 let searchCityElement = document.querySelector("#city");
 searchCityElement.addEventListener("submit", handleSearchSubmit);
