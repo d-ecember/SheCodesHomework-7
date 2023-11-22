3709fd7fccaf3ae10ae728b2a6cdc62b1851508e
d-ecember <119058761+d-ecember@users.noreply.github.com>
2023-11-22 19:51:32 +0100

    Add huminity,Wind,icon and time

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ function updateWeatherData(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let currentCityElement = document.querySelector("#currentCity");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeELement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
   currentCityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeELement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function searchCity(city) {
@@ -17,7 +29,26 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#searchForm");
   searchCity(searchInput.value);
 }
+function formatDate(date) {
+  let minute = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  return `${day} ${hours}:${minute}`;
+}
 let searchCityElement = document.querySelector("#city");
 searchCityElement.addEventListener("submit", handleSearchSubmit);
 
